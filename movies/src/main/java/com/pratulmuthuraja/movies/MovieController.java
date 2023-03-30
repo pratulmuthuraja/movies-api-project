31bcb31ba618fe75355fb3f4fb1aea66f09fa56f
Pratul Muthuraja <pratulmuthuraja@gmail.com>
2023-03-30 18:49:29 -0500

    Add Movie.java. Add MovieController.java. Add Review.java

---
+++ movies/src/main/java/com/pratulmuthuraja/movies/MovieController.java
@@ -0,0 +1,14 @@
+package com.pratulmuthuraja.movies;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+    @GetMapping
+    public String allMovies() {
+        return "All Movies";
+    }
+}
